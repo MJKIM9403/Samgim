5cf32de752b017bc53cc3dba324ce09b3fa493db
admin <kimkh134@gmail.com>
2024-02-19 15:19:04 +0900

    최초 로그인 확인 -> 튜토리얼 로그인 기록 있을 시 -> 메인페이지

--- app/src/main/java/com/example/samgim/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/samgim/ui/welcome/WelcomeActivity.kt
@@ -1,15 +1,17 @@
 package com.example.samgim.ui.welcome
 
+import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.samgim.MainActivity
 import com.example.samgim.R
 
 class WelcomeActivity : AppCompatActivity() {
-
         // 시작하기 버튼 클릭 리스너 설정
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -17,22 +19,19 @@ class WelcomeActivity : AppCompatActivity() {
 
             val welcomeBtn = findViewById<Button>(R.id.welcomeBtn)
 
-
             welcomeBtn.setOnClickListener {
-                val intent = Intent(this, TutorialActivity::class.java)
-                startActivity(intent)
-
-                val decorView: View = window.decorView
-                var uiOption: Int = decorView.systemUiVisibility
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-                    uiOption = uiOption or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                    uiOption = uiOption or View.SYSTEM_UI_FLAG_FULLSCREEN
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-                    uiOption = uiOption or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-
-                decorView.systemUiVisibility = uiOption
+                val pref = this.getSharedPreferences("isFirst", Activity.MODE_PRIVATE)
+                val isFirst = pref.getBoolean("isFirst", true)
+                if(isFirst){
+                    val prefEditor: SharedPreferences.Editor = pref.edit()
+                    prefEditor.putBoolean("isFirst", false)
+                    prefEditor.apply()
+                    val intent = Intent(this, TutorialActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
             }
         }
 
